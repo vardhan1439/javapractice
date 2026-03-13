ee8815d30dab8d1a082dc4f600e67749f8c90e63
vardhan1439 <vardhansanka99@gmail.com>
2026-03-13 08:58:46 +0530

    collections practice

---
+++ src/CollectionsPractice.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.ArrayList;
+
+public class CollectionsPractice {
+
+    public static void main (String[] args) {
+        int a[]= {4,5,5,5,6,6,9,4};
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        for(int i=0;i<a.length;i++) {
+            int k=1;
+            if(al.contains(a[i])){
+                continue;
+            }
+            else{
+                al.add(a[i]);
+                for(int j=i+1;j<a.length;j++){
+                    if(a[i]==a[j]){
+                        k=k+1;
+                    }
+                }
+            }
+            System.out.println(a[i]+" is present "+k+" times");
+        }
+    }
+}
